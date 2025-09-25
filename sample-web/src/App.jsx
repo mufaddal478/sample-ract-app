8de6e9e92eb3a864a3d9b0ae52ee95e8f8585b8d
mufaddal478 <mufaddal.l0478@gmail.com>
2025-09-25 13:28:15 +0530

    initial commit

---
+++ sample-web/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>Hello from React on Azure Blob Static Website 🚀</h1>
+      <p>SPA routing works because 404 is set to index.html.</p>
+    </div>
+  );
+}
